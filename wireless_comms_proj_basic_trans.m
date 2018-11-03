66e208389d93d72cb2f044c9bfdbdf28909dbc48
RaewynD <RaewynD@gmail.com>
2018-11-03 10:34:32 -0400

    cleaned up new transmit signal generation

--- wireless_comms_proj_basic_trans.m
+++ wireless_comms_proj_basic_trans.m
@@ -3,7 +3,6 @@ rng('default'); % Set seed for random number generator (for repeatability of sim
 %user defined values
 qam = 4;
 d = 1;
-L = 200;
 
 %Part A - define constellation
 qam_range = 1:sqrt(qam);
@@ -21,10 +20,9 @@ x1 = [1,1,0,1,0,0,0,1,0,1];
 x_size = size(x1);
 x_size = x_size(2);
 x_range = 1:ceil(x_size/2);
-x_transmitted = [];
 
 x2 = x1;
-x_transmitted_tic = [];
+x_transmitted = [];
 for x = 1:x_size
     if (x2(x) == 0)
         x2(x) = -1;
@@ -33,17 +31,12 @@ end
 
 for x = x_range
     if (x*2 > x_size)
-        x_tic = [x1(x*2 - 1),0];
         x2 = [x2,0];
-    else
-        x_tic = [x1(x*2 - 1),x1(x*2)];
     end
-    x_transmitted = [x_transmitted, pnt_to_const(x_tic, d)];
-    x_transmitted_tic = [x_transmitted_tic, x2(x*2 - 1) + (j * x2(x*2))]
+    x_transmitted = [x_transmitted, x2(x*2 - 1) + (j * x2(x*2))];
 end
 
-x_transmitted
-x_transmitted_tic = 0.5 * d * x_transmitted_tic
+x_transmitted = 0.5 * d * x_transmitted;
 
 transmitsignal = x_transmitted;
 
@@ -51,19 +44,3 @@ save('transmitsignal.mat','transmitsignal')
 
 
 %% ---Helper Functions--- %%
-% translate bits to 4qam
-function x_const = pnt_to_const(x_tic, d_tic)
-    if x_tic(1) == 1
-        if x_tic(2) == 1
-            x_const = 0.5 * d_tic * (1 + 1*j);
-        else
-            x_const = 0.5 * d_tic * (1 - 1*j);
-        end
-    else
-        if x_tic(2) == 1
-            x_const = 0.5 * d_tic * (-1 + 1*j);
-        else
-            x_const = 0.5 * d_tic * (-1 - 1*j);
-        end
-    end
-end
